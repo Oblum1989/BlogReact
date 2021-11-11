a9a98191720efadf266d6aca1a2c52344e21944b
oscar blum <oscar.blum@gmail.com>
2021-11-10 22:45:30 -0500

    lazy load

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,23 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import FourOFour from "../views/404";
-import Expense from "../views/Expense";
-import Pokemons from "../views/Pokemons";
-import PokeDetail from "../views/Pokemons/views/PokeDetail";
-import Home from "../views/Home";
-import Search from "../views/Search";
-import Goals from "../views/Goals";
-import Age from "../views/Age";
-import Login from "../views/Login";
-import NavBar from "../views/UI/Navbar";
-import { useContext, useEffect } from "react";
+import { lazy, Suspense, useContext, useEffect } from "react";
 import PokemonContext from "../context/pokemons";
-import Restaurant from "../views/Restaurant";
-import Quotes from "../views/Quotes";
-import NewQuote from "../views/Quotes/pages/NewQuote";
-import QuoteDetail from "../views/Quotes/pages/QuoteDetail";
-import CartShop from "../views/CartShop";
+import NavBar from "../views/UI/Navbar";
+import Loading from "../components/Loading";
+
+const Goals = lazy(() => import("../views/Goals"))
+const FourOFour = lazy(() => import("../views/404"))
+const Expense = lazy(() => import("../views/Expense"))
+const Pokemons = lazy(() => import("../views/Pokemons"))
+const PokeDetail = lazy(() => import("../views/Pokemons/views/PokeDetail"))
+const Home = lazy(() => import("../views/Home"))
+const Search = lazy(() => import("../views/Search"))
+const Age = lazy(() => import("../views/Age"))
+const Login = lazy(() => import("../views/Login"))
+const Restaurant = lazy(() => import("../views/Restaurant"))
+const Quotes = lazy(() => import("../views/Quotes"))
+const NewQuote = lazy(() => import("../views/Quotes/pages/NewQuote"))
+const QuoteDetail = lazy(() => import("../views/Quotes/pages/QuoteDetail"))
+const CartShop = lazy(() => import("../views/CartShop"))
 
 export default function Routes() {
   const { isLoggedIn, setIsLoggedIn, loginHandler, logoutHandler } =
@@ -31,48 +33,52 @@ export default function Routes() {
   return (
     <Router>
       <NavBar isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <Switch>
-        <Route path="/" exact>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogin={loginHandler} />}
-        </Route>
-        <Route path="/pokemons">
-          <Pokemons />
-        </Route>
-        <Route path="/pokemon/:id">
-          <PokeDetail />
-        </Route>
-        <Route path="/expenses">
-          <Expense />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/goals">
-          <Goals />
-        </Route>
-        <Route path="/age">
-          <Age />
-        </Route>
-        <Route path="/restaurant">
-          <Restaurant />
-        </Route>
-        <Route path="/quotes" exact>
-          <Quotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="/cart">
-          <CartShop />
-        </Route>
-        <Route>
-          <FourOFour />
-        </Route>
-      </Switch>
+      <Suspense fallback={
+        <Loading title="Loading!"/>
+      }>
+        <Switch>
+          <Route path="/" exact>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && <Home onLogin={loginHandler} />}
+          </Route>
+          <Route path="/pokemons">
+            <Pokemons />
+          </Route>
+          <Route path="/pokemon/:id">
+            <PokeDetail />
+          </Route>
+          <Route path="/expenses">
+            <Expense />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/goals">
+            <Goals />
+          </Route>
+          <Route path="/age">
+            <Age />
+          </Route>
+          <Route path="/restaurant">
+            <Restaurant />
+          </Route>
+          <Route path="/quotes" exact>
+            <Quotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="/cart">
+            <CartShop />
+          </Route>
+          <Route>
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
